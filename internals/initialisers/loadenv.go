3f76cad1db72f4dddb87c900b895b8f12ea36309
waltertaya <waltermittybecker5@gmail.com>
2026-03-09 23:57:18 +0300

    improved error handling in LoadEnv function to log specific messages for .env file absence

--- internals/initialisers/loadenv.go
+++ internals/initialisers/loadenv.go
@@ -2,12 +2,18 @@ package initialisers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("No .env found")
+		if os.IsNotExist(err) {
+			log.Println("No .env found, using environment variables from the host")
+			return
+		}
+
+		log.Printf("Failed to load .env: %v", err)
 	}
 }
